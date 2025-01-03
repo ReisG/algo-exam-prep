9d8cf94396f68e54b47c3eb0fd01e8adf4d857cf
ReisG <romanryabcev2006@gmail.com>
2025-01-03 20:54:40 +0300

    alv realization but with wrong memory manipulations (2-pointers needed)

--- AVL-realization/main.c
+++ AVL-realization/main.c
@@ -20,6 +20,7 @@ Avl *create(int key, int val)
     return t;
 }
 
+// show which derection search would go
 int way(Avl *t, int key)
 {
     if (t->key == key) return 0;
@@ -27,11 +28,31 @@ int way(Avl *t, int key)
     else return 1;
 }
 
-Avl *acway(Avl *t, int d)
+// ACcess the WAY derection leads
+acway(Avl *t, int d)
 {
     if (!d) return t;
     else if (d < 0) return t->l;
     else return t->r;
+}
+
+Avl *rotate(Avl *t, int d)
+{
+    if (!d) return t;
+    else if (d < 0)
+    {
+        Avl *tt = t->r;
+        t->r = tt->l;
+        tt->l = t;
+        return tt;
+    }
+    else
+    {
+        Avl *tt = t->l;
+        t->l = tt->r;
+        tt->r = t;
+        return tt;
+    }
 }
 
 Avl *insert(Avl *t, int key, int val)
@@ -41,7 +62,7 @@ Avl *insert(Avl *t, int key, int val)
     Avl *A = NULL;
 
     Avl *p = NULL;
-    while(t && t->key != key)
+    while (t && t->key != key)
     {
         p = t;
         if (!p->bal) A = p;
@@ -55,27 +76,54 @@ Avl *insert(Avl *t, int key, int val)
         return;
     }
 
+    // pushing value to appropiate child
+    // cool construction (I loved it)
     *(key < p->key ? &p->l : &p->r) = create(key, val);
 
-    Avl *r = NULL;
-    if (!A) r = t;
+    Avl *r = NULL; // first vertex to start rebalancing route
+    if (!A) r = t; // there was no unbalanced vertex
     else
     {
-        int d1 = way(p, key);
+        int d1 = way(A, key);
         Avl *C = acway(A, d1);
-        if (A->bal != d1)
+        if (A->bal != d1) // d1 != 0
         {
+            // inserted vertex balanced our vertex back
             A->bal = 0;
-            r = C; // !!!
+            r = C; // not sure, book says p
         }
         else
         {
             int d2 = way(C, key);
             Avl *B = acway(C, d2);
-
+            if (d1 == d2)
+            {
+                A->bal = 0;
+                r = B;
+                rotate(p, -d1);
+            }
+            else
+            {
+                int d3 = way(B, key);
+                r = acway(B, d3);
+                if (d3 == d2)
+                {
+                    A->bal = 0;
+                    C->bal = d1;
+                } else if (d3 == -d2) A->bal = d2;
+                else A->bal = 0;
+                rotate(C, -d2);
+                rotate(A, -d1);
+            }
             
         }
     }
+
+    while (r && r->key != key) r->bal = way(r, key), r = acway(r, r->key - key);
+
+    // return ??? what to return
+    // PROBLEM, we didn't altered real pointers
+    // So after I need to use pointer to pointer
 }
 
 int main(void)
