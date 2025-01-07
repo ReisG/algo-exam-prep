07df44c87d02ad73dc623bdfb82e239521beba84
ReisG <romanryabcev2006@gmail.com>
2025-01-07 23:54:23 +0300

    created z_func implementation

--- KMP-Impl/main.c
+++ KMP-Impl/main.c
@@ -17,11 +17,30 @@ int* pref(char *s)
     return res;
 }
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+int *z_func(char *s)
+{
+    int n = strlen(s);
+    int *res = calloc(n, sizeof(int));
+    int l, r;
+    l = r = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (i < r) res[i] = min(r - i, res[i - l]);
+        while (i + res[i] < n && s[i + res[i]] == s[res[i]]) res[i]++;
+        if (i + res[i] > r)
+        {
+            r = i + res[i];
+            l = i;
+        }
+    }
+    return res;
+}
 
 int main(void)
 {
     char s[] = "abacaba";
-    int *p = pref(s);
+    int *p = z_func(s); //pref(s);
     for (int i = 0; i < strlen(s); i++) printf("%d ", p[i]);
     free(p);
     return 0;
