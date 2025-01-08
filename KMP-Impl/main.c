b5f749f41d1f824f0e75e55a9785dcd866be1037
ReisG <romanryabcev2006@gmail.com>
2025-01-08 10:38:41 +0300

    tried to write pref and z func for the second time

--- KMP-Impl/main.c
+++ KMP-Impl/main.c
@@ -37,10 +37,50 @@ int *z_func(char *s)
     return res;
 }
 
+// repetition
+int *pref2(char *s)
+{
+    int n = strlen(s);
+    int *p = malloc(n*sizeof(int));
+    p[0] = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int j = p[i - 1];
+        while (j > 0 && s[i] != s[j]) j = p[j - 1];
+        if (s[i] == s[j]) j++;
+        p[i] = j;
+    }
+
+    return p;
+}
+
+int *z_func2(char *s)
+{
+    int n = strlen(s);
+    int *z = malloc(n*sizeof(int));
+    z[0] = 0;
+    int l, r;
+    l = r = 0;
+
+    for (int i = 1; i < n; i++)
+    {
+        z[i] = 0;
+        if (i < r) z[i] = min(r - i, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
+        if (i + z[i] > r)
+        {
+            r = i + z[i];
+            l = i;
+        }
+    }
+
+    return z;
+}
+
 int main(void)
 {
     char s[] = "abacaba";
-    int *p = z_func(s); //pref(s);
+    int *p = z_func2(s); // pref2(s); //pref(s);
     for (int i = 0; i < strlen(s); i++) printf("%d ", p[i]);
     free(p);
     return 0;
